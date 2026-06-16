cddbf8ec87094bbb959cfc6a64c30a5e6b56d9a1
Ayush Singh Thakur <singhthakurayush176@gmail.com>
2026-06-16 13:20:14 +0530

    Add files via upload

---
+++ starbynestedloop.c
@@ -0,0 +1,17 @@
+//to create star
+#include<stdio.h>
+void main()
+{
+	int i,j;
+	for(i=1;i<=10;i++)
+	{
+		for(j=1;j<=10;j++)
+		{
+			if(j<=i)
+			printf("*");
+		else
+		    printf(" ");
+		}
+		printf("\n");
+	}
+}
