e5b3e33dea8a64f4e43eb239c81110de3d47f3d4
Ayush Singh Thakur <singhthakurayush176@gmail.com>
2026-06-30 17:05:52 +0530

    Add files via upload

---
+++ volcub.c
@@ -0,0 +1,11 @@
+//to calculate the volume of cuboid
+#include<stdio.h>
+void main()
+{
+	float l,w,h,volume;
+	printf("Enter the value of l,w,h\n");
+	scanf("%f%f%f",&l,&w,&h);
+	volume=l*w*h;
+	printf("the volume of cuboid is %.2f",volume);
+	  
+}
