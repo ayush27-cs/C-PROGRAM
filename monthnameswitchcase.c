acd0f08e6d718980344a60e3553cf82ebca993f3
Ayush Singh Thakur <singhthakurayush176@gmail.com>
2026-07-03 11:23:21 +0530

    Add files via upload

---
+++ monthnameswitchcase.c
@@ -0,0 +1,51 @@
+//to cal the month name
+#include<stdio.h>
+void main()
+{
+	int choice;
+	printf("Enter the value from 1 to 12\n");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:
+			printf("January\n");
+			break;
+			
+		case 2:
+			printf("Febuary\n");
+			break;
+		case 3:
+			printf("March\n");
+			break;
+		case 4:
+			printf("April\n");
+			break;
+		case 5:
+			printf("May\n");
+			break;
+		case 6:
+			printf("June\n");
+			break;
+		case 7:
+			printf("July\n");
+			break;
+		case 8:
+			printf("August\n");
+			break;
+		case 9:
+			printf("September\n");
+			break;
+		case 10:
+			printf("October\n");
+			break;
+		case 11:
+			printf("November\n");
+			break;
+		case 12:
+			printf("December\n");
+			break;
+		default :
+			printf("Error");
+			
+	}
+}
