400f9655ad0ebe5d47e33733155fd2e2243b8e44
Ayush Singh Thakur <singhthakurayush176@gmail.com>
2026-07-08 21:36:55 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+//palindrome number
+#include<stdio.h>
+void main(){
+	int n,original,reverse=0,digit;
+	printf("Emnter the value of n:\n");
+	scanf("%d",&n);
+	while(n>0){
+		digit=n%10;
+		reverse=(reverse*10)+digit;
+		n=n/10;
+		
+	}
+	if(original==reverse)
+	printf("The number is palindrome");
+	else
+	printf("Number is not palindrome");
+}
